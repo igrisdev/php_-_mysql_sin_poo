45ed7b9d9790c08d30c897b9321d2bc6b6f3ea47
igrisdev <johanalvarezz237@gmail.com>
2026-09-23 09:42:21 -0500

    Refactor index.php to conditionally include navbar and scripts based on view validation

--- index.php
+++ index.php
@@ -13,8 +13,17 @@
         $_GET['vista'] = "login";
     }
 
-    include "./inc/navbar.php";
-    include "./inc/script.php";
+    if (is_file("./vistas/" . $_GET['vista'] . ".php") && $_GET["vista"] != "login" && $_GET["vista"] != "404") {
+        include "./inc/navbar.php";
+        include "./vistas/" . $_GET['vista'] . ".php";
+        include "./inc/script.php";
+    } else {
+        if ($_GET["vista"] == "login") {
+            include "./vistas/login.php";
+        } else {
+            include "./vistas/404.php";
+        }
+    }
     ?>
 </body>
 
